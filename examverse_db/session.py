94a64b48c26044704cd7b4875449fc589da65bf8
KnowVerseOS <makashbsp1997@gmail.com>
2026-06-28 14:12:27 +0530

    Create session.py

---
+++ examverse_db/session.py
@@ -0,0 +1,16 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:password@localhost:5432/examverse"
+)
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
